8d481ce8b86241d1f44903ea4fc21ac7d64f85ec
MAC USER <macuser@MACs-iMac.local>
2017-02-09 20:45:30 +0300

    Added one line of code

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Hello World")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
